2bab9c04bc00a5a0398189751a27492eff73d3c7
elliotyandzio <elliotyandzio@gmail.com>
2018-03-04 22:44:02 +0000

    create game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0].toString());
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1].toString());
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You found a match");
-	} else {
-		alert("Sorry try again");
+var checkForMatch = function (){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay [1]) {
+			alert("You found a match");
+		} else {
+			alert("Sorry try again");
+		}
 	}
 }
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
